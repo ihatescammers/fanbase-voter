10a66a093a6fb10a94c2c62dd33bc4fee3508d90
Rayyan Tariq <raytariq93@gmail.com>
2023-09-24 12:37:43 +0500

    .

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,12 +1,21 @@
 import { writable } from 'svelte/store';
 import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { getDoc, updateDoc } from 'firebase/firestore';
 import { app } from './index.js';
+// import 
 
 export const user = writable(null);
 
 const auth = getAuth(app);
-onAuthStateChanged(auth, (newUser) => {
-    user.set(newUser)
+onAuthStateChanged(auth, async (newUser) => {
+    user.set(newUser);
+    if (newUser) {
+        const docRef = doc(db, collection("users"), newUser.uid);
+        const userRef = await getDoc(docRef);
+        await updateDoc(userRef, {
+            votedFor: ''
+        })
+    }
 });
 
 // user.subscribe((value) => {console.log(value)});
